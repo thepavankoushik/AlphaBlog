2bbe3edcdb6a926ec6e1f92dc10a52d92c9a4fd6
pavan koushik <pavankoushik22@gmail.com>
2017-11-06 11:52:56 +0530

    description capacity

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ActiveRecord::Base
 	has_many :article_categories
 	has_many :categories, through: :article_categories
 	validates :title, presence: true, length: {minimum: 3, maximum: 50}
-	validates :description, presence: true, length: {minimum: 10, maximum: 700}
+	validates :description, presence: true, length: {minimum: 10, maximum: 100000000000}
 	validates :user_id, presence: true
 
 end
